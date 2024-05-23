10535a9943f8e1b0faa8aca9a2b9dd4eb5fe6672
EdisionJ <jiangshiwei76@163.com>
2024-05-23 17:21:02 +0800

    use the dag-json codec to convert the Node

--- golangServer/api/utils.go
+++ golangServer/api/utils.go
@@ -1,9 +1,15 @@
 package api
 
 import (
+	"Oracle.com/golangServer/Oracle"
 	"Oracle.com/golangServer/config"
+	"bytes"
 	"context"
+	"encoding/json"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/codec/dagjson"
+	"github.com/ipld/go-ipld-prime/node/basicnode"
 	"log"
 	"math/big"
 )
@@ -23,4 +29,33 @@ func GenTransactOpts(GasLimit uint64) *bind.TransactOpts {
 	auth.GasPrice = gasPrice
 	auth.GasLimit = GasLimit
 	return auth
+}
+
+func nodeTobyte(node ipld.Node) []byte {
+	var buf bytes.Buffer
+	if err := dagjson.Encode(node, &buf); err != nil {
+		log.Fatalf("Trans node to json fail: %v", err)
+	}
+
+	var formatted bytes.Buffer
+	if err := json.Indent(&formatted, buf.Bytes(), "", "  "); err != nil {
+		log.Fatalf("Format json fail: %v", err)
+	}
+	return formatted.Bytes()
+}
+
+func creatNode(event *Oracle.OracleSearch) (node ipld.Node) {
+	switch event.Val.DataType {
+	case "string":
+		node = basicnode.NewString(event.Val.Str)
+	case "int":
+		node = basicnode.NewInt(event.Val.Integer)
+	case "uint":
+		node = basicnode.NewUint(event.Val.Uinteger)
+	case "bytes":
+		node = basicnode.NewBytes(event.Val.Bytess)
+	case "bool":
+		node = basicnode.NewBool(event.Val.Boolean)
+	}
+	return
 }
